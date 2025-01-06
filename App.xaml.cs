177bce57256dd8326238ca605c5ba7acfe2c55ec
cisco <cisco@UPEAULA-31717>
2025-01-06 09:47:05 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WRApiNasa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
