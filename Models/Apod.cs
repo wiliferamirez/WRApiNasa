c139c8202feecc458d42bddc3ad4625240ed0c38
william <william.ramirez.paucar@udla.edu.ec>
2025-01-06 09:53:06 -0500

    create project

---
+++ Models/Apod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRApiNasa.Models
+{
+    internal class Apod
+    {
+        public string date { get; set; }
+        public string explanation { get; set; }
+        public string hdurl { get; set; }
+        public string media_type { get; set; }
+        public string service_version { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
